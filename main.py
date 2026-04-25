3ed2f20e95d8cb6d121a05e8e8e3b61608f1ef59
lucca3447 <lucca3447@hotmail.com>
2026-04-24 21:04:00 -0300

    feat:criado rota home

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI(title="API para cantina", description="API para controle de cantina escolar", version="1.0")
+
+@app.get("/")
+def home():
+    return {
+        "mensagem": "API da cantina funcionandokkkk"
+    }
